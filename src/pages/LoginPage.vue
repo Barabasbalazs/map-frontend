b88c53b663b7331b6657f50f98800e150c6116dc
Barabas Balazs <barabas.balazska@gmail.com>
2024-03-27 19:10:32 +0200

    Added a loader, so there's visual evidence of something working

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,17 +1,22 @@
 <template>
   <PageLayout>
     <div class="flex flex-col items-center h-full w-full px-5 md:w-1/3 gap-6">
-      <LoginForm
-        v-if="loginForm"
-        @register="loginForm = false"
-        @submit="(val) => authenticate(val)"
-      />
-      <RegisterForm
-        v-else
-        @return="loginForm = true"
-        @register="(val) => authenticate(val, false)"
-      />
-      <p v-if="apiError" id="apiError" class="text-red-500">{{ capitalizeFirstLetter(apiError) }}</p>
+      <Loader v-if="isLoading" />
+      <template v-else>
+        <LoginForm
+          v-if="loginForm"
+          @register="loginForm = false"
+          @submit="(val) => authenticate(val)"
+        />
+        <RegisterForm
+          v-else
+          @return="loginForm = true"
+          @register="(val) => authenticate(val, false)"
+        />
+        <p v-if="apiError" id="apiError" class="text-red-500">
+          {{ capitalizeFirstLetter(apiError) }}
+        </p>
+      </template>
     </div>
   </PageLayout>
 </template>
@@ -19,27 +24,31 @@
 import PageLayout from "../components/shared/PageLayout.vue";
 import LoginForm from "../components/auth/LoginForm.vue";
 import RegisterForm from "../components/auth/RegisterForm.vue";
+import Loader from "../components/shared/LoadingAnimation.vue";
 import { User } from "../models/user-model";
 import { useAuthStore } from "../stores/auth-store";
 import router from "../routing/router";
 import { capitalizeFirstLetter } from "../utils/string-manipulation";
 import { ref } from "vue";
 
-const loginForm = ref(true);
-
 const authStore = useAuthStore();
 
+const loginForm = ref(true);
 const apiError = ref("");
+const isLoading = ref(false);
 
 async function authenticate(value: Partial<User>, login = true) {
+  isLoading.value = true;
   apiError.value = "";
   const response = login
     ? await authStore.login(value)
     : await authStore.register(value);
   if (response.hasOwnProperty("message") && "message" in response) {
     apiError.value = response.message;
+    isLoading.value = false;
     return;
   }
+  isLoading.value = false;
   return router.push("/");
 }
 </script>
--- src/pages/TrailsPage.vue
+++ src/pages/TrailsPage.vue
@@ -2,13 +2,16 @@
   <PageLayout>
     <div class="flex flex-col gap-4 items-center">
       <TrailFilters :user="user" @search="getTrails" />
-      <TrailDisplay
-        v-for="trail in trails"
-        :key="trail.id"
-        :trail="trail"
-        :user="user"
-        :editable="user.role === 'guide'"
-      />
+      <Loader v-if="isLoading" />
+      <template v-else>
+        <TrailDisplay
+          v-for="trail in trails"
+          :key="trail.id"
+          :trail="trail"
+          :user="user"
+          :editable="user.role === 'guide'"
+        />
+      </template>
       <TrailDisplay
         v-if="user.role === 'guide'"
         :user="user"
@@ -22,13 +25,16 @@
 import PageLayout from "../components/shared/PageLayout.vue";
 import TrailDisplay from "../components/maps/TrailDisplay.vue";
 import TrailFilters from "../components/trails/TrailFilters.vue";
+import Loader from "../components/shared/LoadingAnimation.vue";
 import { RequestParameters } from "../types/request-parameter";
 import { useAuthStore } from "../stores/auth-store";
 import { useTrailsStore } from "../stores/trails-store";
-import { onMounted, computed } from "vue";
+import { onMounted, computed, ref } from "vue";
 
 const authStore = useAuthStore();
 const trailsStore = useTrailsStore();
+
+const isLoading = ref(false);
 
 const user = computed(() => authStore.user);
 const trails = computed(() => trailsStore.trails);
@@ -43,7 +49,9 @@ async function getTrails(parameters: RequestParameters = {}) {
   // if (user.value.role === "guide") {
   //   parameters.creator = user.value.id;
   // }
+  isLoading.value = true;
   await trailsStore.getTrails(parameters);
+  isLoading.value = false;
 }
 
 onMounted(() => getTrails());
